e96190865e446396d1224d56d0b55c59762b6356
sriman577 <17pa1a0577@vishnu.edu.in>
2020-03-25 20:10:26 +0530

    initial commit

---
+++ TddJunit/src/main/java/Epam_TddJunit/com/TddJunit/App.java
@@ -0,0 +1,13 @@
+package Epam_TddJunit.com.TddJunit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
